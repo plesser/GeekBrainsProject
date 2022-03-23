4d5b5e2a5a4526e6c31c6077bef6c2968391a2f3
Oleg S. Plesser <oleg@plesser.ru>
2022-03-23 11:07:44 +0300

    многозадачность, расчет массива

--- src/homework_12/UnitSpeed.java
+++ src/homework_12/UnitSpeed.java
@@ -16,22 +16,32 @@ public class UnitSpeed {
 
         // ну поехали считать
         long start = System.currentTimeMillis();
-        // формально нам не нужен результат, а нужно только время
-        // поэтому функция нечего не возвращает
+
         Calc calc = new Calc(arr);
         calc.run();;
+        arr = calc.getData();
         long finish = System.currentTimeMillis();
         System.out.println("Затраченное время: " + (finish - start));
+
+        display(arr);
+
+        //System.out.println("step 1 -> " + Arrays.toString(arr));
 
         // заново инициализируем массив
         for (int i=0; i < SIZE; i++){
             arr[i] = 1;
         }
 
+        float[] arr_1 = new float[HALF];
+        float[] arr_2= new float[HALF];
+
         start = System.currentTimeMillis();
 
-        float[] arr_1 = Arrays.copyOfRange(arr, 0, HALF);
-        float[] arr_2 = Arrays.copyOfRange(arr, HALF, SIZE);
+        System.arraycopy(arr, 0, arr_1, 0, HALF);
+        System.arraycopy(arr, HALF, arr_2, 0, HALF);
+
+        //System.out.println(Arrays.toString(arr_1));
+        //System.out.println(Arrays.toString(arr_2));
 
         Calc calc_1 = new Calc(arr_1);
         Calc calc_2 = new Calc(arr_2);
@@ -39,13 +49,27 @@ public class UnitSpeed {
         calc_1.run();
         calc_2.run();
 
-        System.arraycopy(calc_1.getData(), 0, arr, 0, HALF);
-        System.arraycopy(calc_2.getData(), 0, arr, HALF, HALF);
+        arr_1 = calc_1.getData();
+        arr_2 = calc_2.getData();
+
+        //System.out.println(Arrays.toString(arr_1));
+        //System.out.println(Arrays.toString(arr_2));
+
+        System.arraycopy(arr_1, 0, arr, 0, HALF);
+        System.arraycopy(arr_2, 0, arr, HALF, HALF);
+
+        //System.out.println(Arrays.toString(arr));
+
 
         finish = System.currentTimeMillis();
         System.out.println("Затраченное время: " + (finish - start));
 
+        display(arr);
 
+    }
+
+    static void display(float[] arr){
+        System.out.println(arr[0] + " " + arr[HALF] + " " + arr[SIZE-1]);
     }
 
 
@@ -64,7 +88,7 @@ public class UnitSpeed {
         @Override
         public void run() {
             for (int i =0; i < data.length; i++){
-                data[i] = (float) (data[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i /5));
+                data[i] = (float) (data[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i /5) * Math.cos(0.4f + i / 2));
             }
         }
 
